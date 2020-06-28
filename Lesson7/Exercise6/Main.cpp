296524610559e832cd6566c88fccca2ff26ca5ea
SluBard <bard@workforcecenter.slu.edu>
2020-06-28 10:14:54 -0500

    Added Lesson7 Exercise6 from Lesson 7 on debugging

---
+++ Lesson7/Exercise6/Main.cpp
@@ -0,0 +1,37 @@
+#include "Stack.h"
+int main(){
+	try{
+		Stack<int> si;
+
+		displayStackStats<int>(si);
+
+		si.push(22);
+		si.push(426);
+		cout << "Top of stack contains " << si.top() << endl;
+		si.push(57);
+
+		displayStackStats<int>(si);
+		cout << "Top of stack contains " << si.top() << endl;
+
+		si.pop();
+		cout << "Top of statck contains " << si.top() << endl;
+
+		si.pop();
+		displayStackStats<int>(si);
+
+		Stack<char> sc;
+		sc.push('d');
+		sc.push('l');
+		displayStackStats<char>(sc);
+		cout << "Top of char statck contains " << sc.top() << endl;
+	}
+	catch(string str){
+		cout << "Error: "<<str<<endl;
+	}
+	catch(...){
+		cout << "Error: Unexpected exception caught " << endl;
+	}
+	return 0;
+}
+
+
